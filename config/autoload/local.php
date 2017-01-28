85bf1ea6b6422cab6ba252a87d17132492066caa
Bexandy Rodríguez <bexandy@gmail.com>
2017-01-27 20:23:00 -0400

    Framework 2.5.1Development

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -2,7 +2,12 @@
 
 return array(
     'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=test3;host=localhost;charset=utf8',
         'username' => 'root',
-        'password' => 'repede',
+        'password' => 'p3lk4x',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
     ),
 );
